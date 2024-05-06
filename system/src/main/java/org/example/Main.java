355d641de75dbd402d55210e594d4a32bf475f33
xielaoban000 <1150873798@qq.com>
2024-05-05 21:40:57 -0700

    Normalize all the line endings

---
+++ system/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
